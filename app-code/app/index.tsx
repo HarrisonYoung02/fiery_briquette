7d5a407216c8fce62d9baafb656b775ce56dbbc3
Tristan <127442375+tyoung-99@users.noreply.github.com>
2024-05-21 16:13:00 -0400

    Set up 2 template pages, set up framework for android build

--- app-code/app/index.tsx
+++ app-code/app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <Text>Index</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text>Button label</Text>
+        <Button title="Button" onPress={() => router.navigate("/bluetooth")} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
