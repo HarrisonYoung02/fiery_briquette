d5769784aa6d974c2aecbdc70689ae6a4179ab6b
Tristan <127442375+tyoung-99@users.noreply.github.com>
2024-05-23 16:39:46 -0400

    Removed unused imports

--- app-code/hooks/usePushNotifications.tsx
+++ app-code/hooks/usePushNotifications.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import Constants from "expo-constants";
 import { Platform } from "react-native";
 
 export const usePushNotifications = (): Function => {
