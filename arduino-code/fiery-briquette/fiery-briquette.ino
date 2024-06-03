7db6501bf1b16db812a4318359081285755a9adc
Tristan <127442375+tyoung-99@users.noreply.github.com>
2024-06-03 12:30:13 -0400

    Added sample code to verify functionality

---
+++ arduino-code/fiery-briquette/fiery-briquette.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
