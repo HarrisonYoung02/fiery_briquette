be7068511399df523d3ed112de5f0f3b58c1809d
Tristan <127442375+tyoung-99@users.noreply.github.com>
2024-06-11 16:19:43 -0400

    Made status bar black

--- app-code/app/_layout.tsx
+++ app-code/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ statusBarStyle: "dark" }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
